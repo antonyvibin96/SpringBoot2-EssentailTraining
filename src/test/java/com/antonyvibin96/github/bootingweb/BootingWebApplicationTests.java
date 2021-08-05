5ae46904182b6ff46d39901eab8881d72fe086a1
antony-7105 <antony.v@zohocorp.com>
2021-08-05 14:57:58 +0530

    First commit

---
+++ src/test/java/com/antonyvibin96/github/bootingweb/BootingWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.antonyvibin96.github.bootingweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootingWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
